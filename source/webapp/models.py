3e512655c8b59f5ae9abba6ecba3228a4158a6ce
mtashtanovski <mtashtanovski@gmail.com>
2022-01-10 19:02:04 +0600

    Created model Product, registered in admin panel. created base.html and first view index.html. Made migrations.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+CATEGORIES = [('other', 'Разное'),
+              ('laptops', 'Лэптопы'),
+              ('gadgets', 'Гаджеты'),
+              ('tablets', 'Планшеты'),
+              ('cellphones', 'Телефоны')]
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Наименование')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(max_length=20, default='other', choices=CATEGORIES, verbose_name='Категория')
+    balance = models.PositiveIntegerField(default=1, verbose_name="Остаток")
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False, verbose_name='Стоимость')
+
+    def __str__(self):
+        return f" {self.pk}. {self.title}, {self.category}: {self.price}, {self.balance}"
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 # Create your views here.
+def index_view(request):
+    return render(request, 'index.html')
