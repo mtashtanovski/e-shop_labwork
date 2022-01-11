7cdb44391f267b29031fcec8f0efe948d4a4f356
mtashtanovski <mtashtanovski@gmail.com>
2022-01-11 10:48:29 +0600

    Deleted parts of code from previous project, used as template.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -4,18 +4,17 @@ from webapp.models import Product
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    class GuestAdmin(admin.ModelAdmin):
-        list_display = ['id', 'title', 'category', 'balance', 'price']
-        list_filter = ['category', 'title']
-        search_fields = ['title']
-        fields = [
-            'title',
-            'category',
-            'balance',
-            'price',
-            'description',
-        ]
-        readonly_fields = []
+    list_display = ['id', 'title', 'category', 'balance', 'price']
+    list_filter = ['category', 'title']
+    search_fields = ['title']
+    fields = [
+        'title',
+        'category',
+        'balance',
+        'price',
+        'description',
+    ]
+    readonly_fields = []
 
 
 admin.site.register(Product, ProductAdmin)
