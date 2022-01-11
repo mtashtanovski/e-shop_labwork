abd7af4104c6d7d64bfe261c6c15bb942cf1871f
mtashtanovski <mtashtanovski@gmail.com>
2022-01-11 18:57:03 +0600

    Finished Etap 3. Created functions of deleting, updating and adding of products. Also created forms.py with all necessary fields. Modified views and urls files due to last changes. Modified all pages into Russian language.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
 from webapp.views import (index_view,
-                          product_view
+                          product_view,
+                          add_product,
+                          update_product,
+                          delete_product
                           )
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('product/<int:pk>/', product_view, name='product_view' )
+    path('product/<int:pk>/', product_view, name='product_view'),
+    path('product/add/', add_product, name='add_product'),
+    path('product/<int:pk>/update/', update_product, name='update_product'),
+    path('product/<int:pk>/delete/', delete_product, name='delete_product')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from webapp.models import Product
+from webapp.forms import ProductForm
 
 
 def index_view(request):
@@ -12,3 +13,54 @@ def index_view(request):
 def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product_view.html', {'product': product})
+
+
+def add_product(request):
+    if request.method == 'GET':
+        form = ProductForm()
+        return render(request, 'add_product.html', {"form": form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            description = form.cleaned_data.get('description')
+            price = form.cleaned_data.get('price')
+            category = form.cleaned_data.get('category')
+            balance = form.cleaned_data.get('balance')
+            new_product = Product.objects.create(title=title, description=description,
+                                                 price=price, category=category, balance=balance)
+            return redirect('index')
+        return render(request, 'add_product.html', {"form": form})
+
+
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "GET":
+        form = ProductForm(initial={
+            'title': product.title,
+            'description': product.description,
+            'price': product.price,
+            'category': product.category,
+            'balance': product.balance
+        })
+        return render(request, 'update_product.html', {'product': product, 'form': form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.title = request.POST.get('title')
+            product.description = request.POST.get('description')
+            product.price = request.POST.get('price')
+            product.category = request.POST.get('category')
+            product.balance = request.POST.get('balance')
+            product.save()
+            return redirect('index')
+        return render(request, 'update_product.html', {'product': product, 'form': form})
+
+
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "GET":
+        return render(request, 'delete_product.html', {'product': product})
+    else:
+        product.delete()
+        return redirect('index')
