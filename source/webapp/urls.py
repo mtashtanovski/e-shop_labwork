3c0b9df2323b945fb9297908c803c4a7ac7cb877
mtashtanovski <mtashtanovski@gmail.com>
2022-01-11 15:25:15 +0600

    Etap 2 completed. Modified base.html and index.html. Created product_view.html page. Modified urls and views.py.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from webapp.views import (index_view,
-
+                          product_view
                           )
 
 urlpatterns = [
-    path('', index_view, name='index')
+    path('', index_view, name='index'),
+    path('product/<int:pk>/', product_view, name='product_view' )
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-
 # Create your views here.
+from webapp.models import Product
+
+
 def index_view(request):
-    return render(request, 'index.html')
+    products = Product.objects.order_by('category', '-title')
+    return render(request, 'index.html', {'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', {'product': product})
